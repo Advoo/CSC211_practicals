e1d2dca50707918a56d6e473e480a57f2fd959ea
Advoo <advocatekarabo09@gmail.com>
2026-02-09 15:56:53 -0800

    Refined ADDED SlowShuffle method

--- SlowShuffle.java
+++ SlowShuffle.java
@@ -1,20 +1,38 @@
 import java.util.*;
 
+
+
+
+import java.util.Random;
+
 public class SlowShuffle {
 
-    public static void main(String[] args) {
-        int N = 10;
-        boolean[] used = new boolean[N + 1];
-        Random rand = new Random();
+    public static int[] slowShuffle(int N) {
+        int[] shuffled = new int[N];
+        boolean[] isNotPresent = new boolean[N + 1];
 
-        int count = 0;
-        while (count < N) {
-            int x = rand.nextInt(N) + 1;
-            if (!used[x]) {
-                used[x] = true;
-                System.out.print(x + " ");
-                count++;
+        for (int i = 1; i <= N; i++) {
+            isNotPresent[i] = true;
+        }
+
+        Random rand = new Random();
+        int i = -1;
+
+        while (i < N - 1) {
+            int r = rand.nextInt(N) + 1; // [1..N]
+            if (isNotPresent[r]) {
+                i++;
+                shuffled[i] = r;
+                isNotPresent[r] = false;
             }
+        }
+        return shuffled;
+    }
+
+    public static void main(String[] args) {
+        int[] result = slowShuffle(10);
+        for (int x : result) {
+            System.out.print(x + " ");
         }
     }
 }
