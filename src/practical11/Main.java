521c400e1919871a68168dead84f70cb63ab1d44
Advoo <advocatekarabo09@gmail.com>
2026-03-06 04:07:41 -0800

    Created main method , and test N= 3

--- src/practical11/Main.java
+++ src/practical11/Main.java
@@ -1,11 +1,21 @@
 package practical11;
 
-import java.utiil.Arrays;
+import javax.print.attribute.standard.PrinterMakeAndModel;
+import java.util.Arrays;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main{
     public static void main(String[] agrs){
-        System.out.println("Hello advocate");
+        System.out.println("Comparison of methods");
+        int N = 3; //example size
+        System.out.println("Slow shuffle: ");
+        int[] slow = slowshuffle(N);
+        System.out.println(Arrays.toString(slow));
+
+        System.out.println("Biased shuffle:");
+        int[] biased = biasedShuffle(N);
+        System.out.println(Arrays.toString(biased));
     }
 
     // Slow shuffle using improved method
@@ -20,7 +30,7 @@ public class Main{
             if (isNotPresent[r]) {
                 shuffled[i] = r;
                 isNotPresent[r] = false;
-                i++
+                i++;
             }
         }
 
@@ -38,12 +48,15 @@ public class Main{
 
     // Biased Shuffle
     public static int[] biasedShuffle(int N) {
-        int[] shuffled new int[N];
+        int[] shuffled = new int[N];
+        for(int i = 0; i < N; i++) shuffled[i] = i + 1;
+        Random rand = new Random();
         for (int i = 0; i < N; i++) {
             int ri = rand.nextInt(N);
             int temp = shuffled[i];
-            shuffled[i] = shuffles[ri];
+            shuffled[i] = shuffled[ri];
             shuffled[ri] = temp;
         }
+        return shuffled;
     }
 }
