1bbfff934fc178d629733ba2fb0824e1f4cb4052
Advoo <advocatekarabo09@gmail.com>
2026-03-06 03:47:15 -0800

    Implemente slow shuffle method

--- src/practical11/Main.java
+++ src/practical11/Main.java
@@ -1,5 +1,29 @@
+package practical11;
+
+import java.utiil.Arrays;
+import java.util.Random;
+
 public class Main{
     public static void main(String[] agrs){
         System.out.println("Hello advocate");
+    }
+
+    // Slow shuffle using improved method
+    public static int[] slowshuffle(int N){
+        int[] shuffled = new int[N];
+        boolean[] isNotPresent = new boolean[N + 1];
+        Arrays.fill(isNotPresent, true);
+        Random rand = new Random();
+        int i  = 0;
+        while (i < N - 1) {
+            int r = rand.nextInt(N) + 1;
+            if (isNotPresent[r]) {
+                shuffled[i] = r;
+                isNotPresent[r] = false;
+                i++
+            }
+        }
+
+        //last number
     }
 }
