ca58d8f5fbed6fed79a2ee6f8489a387a1347c4e
Advoo <advocatekarabo09@gmail.com>
2026-03-07 21:57:35 -0800

    Implement main to run the mathods and create the table

--- src/practical12/Main.java
+++ src/practical12/Main.java
@@ -2,10 +2,53 @@ package practical12;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] agrs){
+    public static void main(String[] agrs) {
 
-        // main example
+        // main
+        Random rand = new Random();
 
+        int[] ns = {10, 20, 50, 100, 200, 500,
+                1000, 2000, 5000, 10000, 20000
+        };
+
+        System.out.println("%-8s %-15s %-15s %-15s %-15s%n\",\n" +
+                "                \"n\", \"O(n³)\", \"O(n²A)\", \"O(n²B)\", \"O(n)");
+
+        for (int n : ns) {
+            int[] X = generateArray(n, rand);
+
+            long count3 = -1;
+            long count2A, count2B, count1;
+
+            //o(n^3) - only for n <= 500 to avoid extreme runtime
+
+            if (n <= 500) {
+                count = 0;
+                mcsOn3(X);
+                count3 = count;
+            }
+
+            //o(n^2) A
+
+            count = 0;
+            mcsOn2A(X);
+            count2A = count;
+
+            //B
+            count = 0;
+            mcsOn2B(X);
+            count2B = count;
+
+            //o(n)
+            count = 0;
+            mcsOn(X);
+            count1 = count;
+
+
+            System.out.printf("%-8d %-15d %-15d %-15d %-15d%n",
+                    n, count3, count2A, count2B, count1);
+
+        }
     }
     private static long count; //Counter used by all algorithms - reset before each call
 
@@ -99,6 +142,8 @@ public class Main {
         return X;
 
         }
+
+
 
     }
 
