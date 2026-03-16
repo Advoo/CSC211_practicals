551659b2a90727e37cdde991921f1be676322414
Advocate Karabo <advocatekarabo09@gmail.com>
2026-03-16 15:22:25 -0700

    Implemented Heap Sort class

--- src/practical16/HeapSort.java
+++ src/practical16/HeapSort.java
@@ -1,4 +1,15 @@
 package practical16;
 
 public class HeapSort {
+    public static void heapSort(String[] array) {
+
+        Heap heap = new Heap(array);
+        heap.buildHeapBottomUp();
+
+        for (int i = array.length - 1; i > 0; i--){
+            String temp= array[0];
+            array[0] = array[i];
+            array[i] + temp;
+        }
+    }
 }
