f1f098307393db9d4c60ada2da697b4985dfeaae
Advocate Karabo <advocatekarabo09@gmail.com>
2026-03-23 15:00:20 -0700

    Build a perfect balanced BST from numbers

--- src/practical17/BTS.java
+++ src/practical17/BTS.java
@@ -3,11 +3,33 @@ package practical17;
 public class BTS {
 
     public tNode insert(tNode root, int key){
+        if (root == null) return new tNode(key); //if null position, insert new node
+
+        if (key < root.key)
+            root.left = insert(root.left, key);
+        else
+            root.right = insert(root.right, key);
+
+        return root;
 
     }
     public tNode buildBalanced(int start, int end){
+
+        if (start > end) return null; //base case
+
+        int mid = (start + end ) / 2;
+
+        tNode node = new tNode(mid);
+
+        node.left = buildBalanced(start, mid -1);
+
+        node.right = buildBalanced((mid + 1, end);
+
+        return node;
     }
     public boolean inBST(tNode node, int min, int max){
+
+
     }
     public tNode delete(tNode root , int key){
 
