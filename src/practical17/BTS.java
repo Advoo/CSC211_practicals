a1bee96461d627e1e8c244304e8532ab9f0a5cf7
Advocate Karabo <advocatekarabo09@gmail.com>
2026-03-23 14:21:21 -0700

    Create the interfaces for BTS without implementation

---
+++ src/practical17/BTS.java
@@ -0,0 +1,22 @@
+package practical17;
+
+public class BTS {
+
+    public tNode insert(tNode root, int key){
+
+    }
+    public tNode buildBalanced(int start, int end){
+    }
+    public boolean inBST(tNode node, int min, int max){
+    }
+    public tNode delete(tNode root , int key){
+
+    }
+    private tNode findMin(tNode node){
+
+    }
+    public tNode removeEvens(tNode root){
+
+    }
+
+}
